fd4a2554645dd6cbf2b15540941f96fa9e6caace
rohithchowdary2001 <rohithyadlapalli2001@gmail.com>
2024-01-22 20:13:01 -0600

    first commits

---
+++ MS OOP/src/Helloexample.java
@@ -0,0 +1,9 @@
+
+public class Helloexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Rohith");
+	}
+
+}
---
+++ MS OOP/src/example/Project_Emergency.java
@@ -0,0 +1,9 @@
+package example;
+
+public class Project_Emergency {
+
+	public static void main(String[] args) {
+        System.out.println("An Emergency Broadcast");
+	}
+
+}
